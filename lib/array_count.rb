716c205df9eaec1a3a6ab30570b3884d2042d0d4
Maysoon Tabali <mtab03@me.com>
2020-07-21 21:30:32 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -9,6 +9,6 @@ end
 def count_empty_strings(array)
   array.count do |element|
   element.class == String && element.empty?
-end
+  end
 end 
 # Return the total number of EMPTY strings in the provided array using 
